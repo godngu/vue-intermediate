5fe783d056270caeab3c2d976dde36af6b2d034f
kil <godngu@gmail.com>
2021-05-27 11:24:48 +0900

    Vuex state 적용 리팩토링

--- vue-todo/src/components/TodoList.vue
+++ vue-todo/src/components/TodoList.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <transition-group name="list" tag="ul">
-            <li v-for="(todoItem, index) in propsdata" v-bind:key="todoItem.item" class="shadow">
+            <li v-for="(todoItem, index) in this.$store.state.todoItems" v-bind:key="todoItem.item" class="shadow">
                 <i class="fas fa-check checkBtn" @click="toggleComplete(todoItem, index)"
                    v-bind:class="{checkBtnCompleted: todoItem.completed}"></i>
                 <span v-bind:class="{textCompleted: todoItem.completed}">{{ todoItem.item }}</span>
--- vue-todo/src/store/store.js
+++ vue-todo/src/store/store.js
@@ -3,5 +3,25 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex);
 
+const storage = {
+    fetch() {
+        const arr = [];
+        if (localStorage.length > 0) {
+            for (let i = 0; i < localStorage.length; i++) {
+                if (localStorage.key(i) !== 'loglevel:webpack-dev-server') {
+                    const str = localStorage.getItem(localStorage.key(i));
+                    const obj = JSON.parse(str);
+                    arr.push(obj);
+                }
+            }
+        }
+        return arr;
+    }
+}
+
 export const store = new Vuex.Store({
+    state: {
+        headerText: 'TODO it!',
+        todoItems: storage.fetch()
+    }
 });
